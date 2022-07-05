2575f9f3970dfebb79f2ee94c738ae3d00ba9058
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-07-05 14:58:47 +0300

    components are done

--- src/App.js
+++ src/App.js
@@ -8,7 +8,24 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
-  const fetchTours = async ()=>
+  const fetchTours = async ()=>{
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();   
+  }, []);
+  
+
 
   if (loading) {
     return(
@@ -19,7 +36,7 @@ function App() {
   }
 
   return <main>
-    <Tours/>
+    <Tours tours={tours}/>
   </main>
 }
 
